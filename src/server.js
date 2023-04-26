6609def10af7e3b7bdba9a959950a87317c681a2
iah <ceu@papl.co>
2023-04-26 12:43:49 -0300

    Update CHANGELOG.md

--- src/server.js
+++ src/server.js
@@ -106,6 +106,7 @@ app.listen(PORT, () => {
 function makeTypes(types) {
   console.log(types)
   return types?.data?.map(type => {
+    console.log(type)
     return `${type.attributes.parent?.data.attributes.abbreviation}-${type.id}`;
   }).join(', ');
 }
