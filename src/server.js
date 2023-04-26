d8c298a233003a0ae266b99174edf3677c09234b
iah <ceu@papl.co>
2023-04-26 11:04:41 -0300

    Update CHANGELOG.md

--- src/server.js
+++ src/server.js
@@ -5,14 +5,27 @@ const { exec } = require('child_process');
 
 const app = express();
 
+function extractNetworkData(task) {
+  return task.attributes.network.data.attributes;
+}
+
 app.get('/generate-changelog', async (req, res) => {
   try {
-    const { data: { data: tasks } } = await axios.get('http://localhost:1337/api/tasks?populate=*');
-    
-    
+    const {
+      data: { data: tasks },
+    } = await axios.get('http://localhost:1337/api/tasks?populate=*');
+
+    const tasksByChainId = {};
+
     for (const task of tasks) {
-      const chainId = task.attributes.network.data.attributes.chain_id;
-      console.log(chainId)
+      const chainId = extractNetworkData(task).chain_id;
+      if (!tasksByChainId[chainId]) {
+        tasksByChainId[chainId] = [];
+      }
+      tasksByChainId[chainId].push(task);
+    }
+
+    for (const chainId in tasksByChainId) {
       const outputDir = `./output/${chainId}`;
 
       await fs.ensureDir(outputDir);
@@ -20,22 +33,25 @@ app.get('/generate-changelog', async (req, res) => {
       const changelogPath = `${outputDir}/CHANGELOG.md`;
       await fs.ensureFile(changelogPath);
 
-      const changelogContent = tasks
-        .filter(t => task.attributes.network.data.attributes.chain_id === chainId)
-        .map(t => `- Task ID: ${t.id}`)
+      const changelogContent = tasksByChainId[chainId]
+        .map((task) => `- Task ID: ${task.id}`)
         .join('\n');
 
       await fs.writeFile(changelogPath, changelogContent);
-
-      exec('git add . && git commit -m "Update CHANGELOG.md" ', (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
-      });
     }
+
+    exec('git add . && git commit -m "Update CHANGELOG.md" && git push', (error, stdout, stderr) => {
+      if (error) {
+        if (error.message.includes('index.lock')) {
+          console.error('Another Git process is running, or the lock file was not properly removed. Please try again later.');
+        } else {
+          console.error(`exec error: ${error}`);
+        }
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
 
     res.status(200).send('Changelogs generated and pushed to the repository');
   } catch (error) {
